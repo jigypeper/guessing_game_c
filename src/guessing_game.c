65ef99cce70eb300f98366f09a2a395d03481282
Ahmed Hagi <ahmedosman@mac.com>
2025-11-01 11:51:56 +0000

    fix: flag for showing losing message

--- src/guessing_game.c
+++ src/guessing_game.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 int main(void) {
   int random_number, guess, count = 0;
+  bool won = false;
   srand(time(0));
   random_number = rand() % 10 + 1;
   printf("Guess a number between 1 and 10\n");
@@ -14,6 +16,7 @@ int main(void) {
     scanf("%d", &guess);
     if (guess == random_number) {
       printf("You got it right!\n");
+      won = true;
       break;
     } else {
       printf("Wrong! Guess again\n");
@@ -21,7 +24,9 @@ int main(void) {
     ++count; 
   }
 
-  printf("Out of guesses, you lose!\n");
+  if (!won) {
+    printf("Out of guesses, you lose!\n");
+  }
 
   return 0;
 }
